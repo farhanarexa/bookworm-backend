db601be4e92a5c396369698c9eacb7b7e719d0c0
farhanarexa <mailfarhana702@gmail.com>
2026-01-12 23:39:19 +0600

    add user routes for registration, login, logout, and library management

---
+++ routes/userRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const {
+    registerUser,
+    loginUser,
+    logoutUser,
+    getUserProfile
+} = require('../controllers/authController');
+const { addToShelf, getUserLibrary } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
+
+router.post('/', registerUser);
+router.post('/login', loginUser);
+router.post('/logout', logoutUser);
+router.get('/profile', protect, getUserProfile);
+router.post('/shelf', protect, addToShelf);
+router.get('/library', protect, getUserLibrary);
+
+module.exports = router;
